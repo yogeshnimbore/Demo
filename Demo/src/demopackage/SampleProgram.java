5305b62a52b6c035d709fcf4674635a18e1aabb6
yogeshnimbore <yogeshnimbore99@gmail.com>
2022-07-11 17:13:36 +0530

    Commit 1

---
+++ Demo/src/demopackage/SampleProgram.java
@@ -0,0 +1,22 @@
+package demopackage;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		
+		// Boolean Array - Boolean Data type
+		boolean b[] = new boolean[3];
+		
+		b[0]=true;
+		b[1]=false;
+		
+		System.out.println(b.length);
+		System.out.println();
+		
+		System.out.println(b[0]);
+		System.out.println(b[1]);
+		System.out.println(b[2]);
+
+	}
+
+}
